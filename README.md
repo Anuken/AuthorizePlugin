a2efcbc3f6778db5e351cb79e119cbba28ecf34b
Anuken <arnukren@gmail.com>
2020-11-01 19:28:29 -0500

    6.0

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ### Commands
 
-- `authorize <add/remove> <player>`: Makes a player authorized/unauthorized. Authorized players can break/place/configure blocks, unauthorized ones cannot.
-- `authorize-message <message>`: Sets the message that is displayed when someone tries to interact with the world.
+- `auth <add/remove> <player>`: Makes a player authorized/unauthorized. Authorized players can break/place/configure blocks, unauthorized ones cannot.
+- `auth-message <message>`: Sets the message that is displayed when someone tries to interact with the world.
+- `auth-units <on/off>`: Sets whether players need authorization in order to control or command units..
 
 ### Downloading
 
--- src/authorize/AuthorizePlugin.java
+++ src/authorize/AuthorizePlugin.java
@@ -4,10 +4,9 @@ import arc.*;
 import arc.struct.*;
 import arc.util.*;
 import mindustry.*;
-import mindustry.entities.type.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
 import mindustry.net.Administration.*;
-import mindustry.plugin.*;
-import mindustry.world.blocks.units.*;
 
 public class AuthorizePlugin extends Plugin{
     public static final float messageSpacing = 60f;
@@ -15,15 +14,20 @@ public class AuthorizePlugin extends Plugin{
     private ObjectSet<String> authorized = new ObjectSet<>();
     private ObjectFloatMap<Player> messageTime = new ObjectFloatMap<>();
     private String message = "[scarlet]You are not authorized to perform this action.";
+    private boolean authUnits = true;
 
     @Override
     public void init(){
-        authorized = Core.settings.getObject("authorized-list", ObjectSet.class, ObjectSet::new);
+        authorized = Core.settings.getJson("authorized-players", ObjectSet.class, String.class, ObjectSet::new);
         message = Core.settings.getString("authorized-message", message);
+        authUnits = Core.settings.getBool("allow-unauthorized-units", true);
 
         Vars.netServer.admins.addActionFilter(action -> {
             if(action.player == null) return true;
-            if(action.player.isAdmin || authorized.contains(action.player.usid) || (action.type == ActionType.tapTile && action.tile.block() instanceof MechPad)){
+            if(action.player.admin || authorized.contains(action.player.usid()) ||
+                (authUnits && (action.type == ActionType.control || action.type == ActionType.command)) || //check if they can command units
+                (action.type == ActionType.control && action.unit == null) //make sure they can un-control units
+            ){
                 return true;
             }else{
                 message(action.player);
@@ -34,20 +38,20 @@ public class AuthorizePlugin extends Plugin{
 
     @Override
     public void registerServerCommands(CommandHandler handler){
-        handler.register("authorize", "<add/remove> <player...>", "Authorize or unauthorize player by name or UUID.", arg -> {
-            Player player = Vars.playerGroup.find(p -> p.uuid.equals(arg[1]) || Strings.stripColors(p.name).equals(Strings.stripColors(arg[1])));
+        handler.register("auth", "<add/remove> <player...>", "Authorize or unauthorize player by name or UUID.", arg -> {
+            Player player = Groups.player.find(p -> p.uuid().equals(arg[1]) || Strings.stripColors(p.name).equals(Strings.stripColors(arg[1])));
             if(arg[0].equals("add")){
                 if(player != null){
-                    authorized.add(player.usid);
-                    Log.info("Authorized: &ly{0}", player.name);
+                    authorized.add(player.usid());
+                    Log.info("Authorized: @", player.name);
                     save();
                 }else{
                     Log.err("Player not found. Note that they must be online for authorization to work.");
                 }
             }else if(arg[0].equals("remove")){
                 if(player != null){
-                    authorized.remove(player.usid);
-                    Log.info("Un-authorized: &ly{0}", player.name);
+                    authorized.remove(player.usid());
+                    Log.info("Un-authorized: @", player.name);
                     save();
                 }else{
                     Log.err("Player not found. Note that they must be online for authorization to work.");
@@ -57,13 +61,23 @@ public class AuthorizePlugin extends Plugin{
             }
         });
 
-        handler.register("authorize-message", "[message...]", "Set the message displayed when someone is not authorized to perform an action.", arg -> {
+        handler.register("auth-message", "[message...]", "Set the message displayed when someone is not authorized to perform an action.", arg -> {
             if(arg.length > 0){
                 message = arg[0];
                 save();
                 Log.info("Message set.");
             }else{
-                Log.info("Current message: {0}", message);
+                Log.info("Current message: @", message);
+            }
+        });
+
+        handler.register("auth-units", "[yes/no]", "Set whether unauthorized players are able to control units. Default: yes.", arg -> {
+            if(arg.length > 0){
+                authUnits = arg[0].equals("yes");
+                save();
+                Log.info("auth-units set to '@'.", arg[0].equals("yes") ? "yes" : "no");
+            }else{
+                Log.info("Current value: @", authUnits ? "yes" : "no");
             }
         });
     }
@@ -82,7 +96,7 @@ public class AuthorizePlugin extends Plugin{
 
     private void save(){
         Core.settings.put("authorized-message", message);
-        Core.settings.putObject("authorized-list", authorized);
-        Core.settings.save();
+        Core.settings.put("allow-unauthorized-units", authUnits);
+        Core.settings.putJson("authorized-list", String.class, authorized);
     }
 }
